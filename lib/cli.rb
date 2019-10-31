74e3b9b8c884f23b8701db3c78a561a5671195ec
Geoff Elliott <tinywv@gmail.com>
2019-10-31 02:40:53 +0000

    built out classes

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,23 +1,25 @@
 require 'nokogiri'
 require 'open-uri'
 
-def run
-  puts "Welcome to IMDB Openings!"
-  puts "These are the movies opening this week:"
-  puts " "
-  puts ": ======================= :"
-  puts " "
-
-  html = open("https://www.imdb.com/")
-  doc = Nokogiri::HTML(html)
-  movies = []
-  doc.css("div.rhs-body")[0].("div.rhs-row").each do |mov|
-    title = mov.css(".title").text.strip
-    movies << title
+class CLI
+  def run
+    puts "Welcome to IMDB Openings!"
+    puts "These are the movies opening this week:"
+    puts " "
+    puts ": ======================= :"
+    puts " "
+  
+    html = open("https://www.imdb.com/")
+    doc = Nokogiri::HTML(html)
+    movies = []
+    doc.css("div.rhs-body")[0].("div.rhs-row").each do |mov|
+      title = mov.css(".title").text.strip
+      movies << title
+    end
+    
+    movies.each.with_index(1) do |title, index|
+      puts "#{index}. #{title}"
+    end 
   end
   
-  movies.each.with_index(1) do |title, index|
-    puts "#{index}. #{title}"
-  end
-
 end
