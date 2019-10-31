901c396b4a6fa6620c2ade906bc377678542a584
Geoff Elliott <tinywv@gmail.com>
2019-10-31 00:27:05 +0000

    first commit

---
+++ lib/cli/nancy.rb
@@ -0,0 +1,8 @@
+require "cli/nancy/version"
+
+module Cli
+  module Nancy
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/cli/nancy/version.rb
@@ -0,0 +1,5 @@
+module Cli
+  module Nancy
+    VERSION = "0.1.0"
+  end
+end
